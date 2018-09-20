34ea169d1740192f1451862b2909fdacebbdb6bc
Josh Kasten <josh@onesignal.com>
2018-09-20 14:09:35 -0700

    Update to Gradle 4.10.2

--- src/test/groovy/com/onesignal/androidsdk/GradleTestTemplate.groovy
+++ src/test/groovy/com/onesignal/androidsdk/GradleTestTemplate.groovy
@@ -21,7 +21,7 @@ class GradleTestTemplate {
     // Set to '--info' or '--stacktrace' to debug issues
     static def GRADLE_LOG_LEVEL = null
 
-    static def GRADLE_LATEST_VERSION = '4.9'
+    static def GRADLE_LATEST_VERSION = '4.10.2'
     static def GRADLE_OLDEST_VERSION = '2.14.1'
 
     static void setup() {
