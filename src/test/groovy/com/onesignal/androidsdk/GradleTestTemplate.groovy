15ec4e52b54d133aed2a0a57179a9d6f784d7f3f
Josh Kasten <josh@onesignal.com>
2019-05-22 10:35:35 -0700

    Updated to Gradle 5.4.1 and AGP 3.4.1
    - Both in compile and in unit tests

--- src/test/groovy/com/onesignal/androidsdk/GradleTestTemplate.groovy
+++ src/test/groovy/com/onesignal/androidsdk/GradleTestTemplate.groovy
@@ -21,13 +21,13 @@ class GradleTestTemplate {
     // Set to '--info' or '--stacktrace' to debug issues
     static def GRADLE_LOG_LEVEL = null
 
-    static def GRADLE_LATEST_VERSION = '4.10.2'
+    static def GRADLE_LATEST_VERSION = '5.4.1'
     static def GRADLE_OLDEST_VERSION = '2.14.1'
 
     static void setup() {
         gradleVersions = [
             (GRADLE_OLDEST_VERSION): 'com.android.tools.build:gradle:2.2.3',
-            (GRADLE_LATEST_VERSION): 'com.android.tools.build:gradle:3.3.0'
+            (GRADLE_LATEST_VERSION): 'com.android.tools.build:gradle:3.4.1'
         ]
 
         buildArgumentSets = [
