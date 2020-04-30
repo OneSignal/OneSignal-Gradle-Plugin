fb3943c7c9b34b61ed271dc68f0ba9c6f6defc77
Josh Kasten <josh@onesignal.com>
2020-04-30 03:41:59 -0700

    Updated to Gradle 6.3

--- src/test/groovy/com/onesignal/androidsdk/MainTest.groovy
+++ src/test/groovy/com/onesignal/androidsdk/MainTest.groovy
@@ -27,7 +27,7 @@ class MainTest extends Specification {
 
         then:
         results.each {
-            assert it.value.contains('com.onesignal:OneSignal:[3.8.3, 3.99.99] -> 3.13.0')
+            assert it.value.contains('com.onesignal:OneSignal:[3.8.3, 3.99.99] -> 3.13.2')
         }
     }
 
