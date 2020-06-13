6f1ca59ae7152cac92c12240ab6c1a9463625811
Josh Kasten <josh@onesignal.com>
2020-06-13 05:13:29 -0700

    Upgraded to Gradle 6.5
    * Ran `gradlew wrapper --gradle-version=6.5 --distribution-type all`
    * Also updated test template

--- src/test/groovy/com/onesignal/androidsdk/GradleTestTemplate.groovy
+++ src/test/groovy/com/onesignal/androidsdk/GradleTestTemplate.groovy
@@ -21,7 +21,7 @@ class GradleTestTemplate {
     // Set to '--info' or '--stacktrace' to debug issues
     static def GRADLE_LOG_LEVEL = null
 
-    static def GRADLE_LATEST_VERSION = '6.4'
+    static def GRADLE_LATEST_VERSION = '6.5'
     static def GRADLE_OLDEST_VERSION = '2.14.1'
 
     static void setup() {
