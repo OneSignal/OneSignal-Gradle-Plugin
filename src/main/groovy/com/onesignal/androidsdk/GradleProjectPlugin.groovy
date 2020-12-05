e3dbd8651280eb756eca87da6746fc271fec6790
Josh Kasten <josh@onesignal.com>
2020-12-05 15:24:18 -0800

    Checked and Updated all Google module min versions

--- src/main/groovy/com/onesignal/androidsdk/GradleProjectPlugin.groovy
+++ src/main/groovy/com/onesignal/androidsdk/GradleProjectPlugin.groovy
@@ -167,14 +167,14 @@ class GradleProjectPlugin implements Plugin<Project> {
             '16.0.0': [
                 'com.google.firebase:firebase-messaging': '17.0.0'
             ],
-            // Tested up to firebase-core:17.5.1
+            // Tested up to firebase-core:18.0.0
         ],
         'com.google.firebase:firebase-common': [
             // Tested firebase-common:17.1.0 back to firebase-iid:10.2.1
             '18.0.0': [
                 'com.google.firebase:firebase-iid': '19.0.0'
             ]
-            // Tested up to firebase-common:19.3.0
+            // Tested up to firebase-common:19.4.0
         ],
         'com.google.firebase:firebase-iid': [
             '16.2.0': [
@@ -231,7 +231,12 @@ class GradleProjectPlugin implements Plugin<Project> {
         'com.google.android.gms:play-services-measurement-base': [
             '15.0.4': [
                 'com.google.firebase:firebase-analytics': '16.0.0'
-            ]
+            ],
+            // NOTE: Versions in-between untested
+            '18.0.0': [
+                'com.google.firebase:firebase-analytics': '18.0.0'
+            ],
+            // Tested up to play-services-measurement-base:18.0.0
         ],
         'com.google.android.gms:play-services-basement': [
             '16.0.1': [
@@ -244,7 +249,7 @@ class GradleProjectPlugin implements Plugin<Project> {
             '17.1.0': [
                 'com.google.firebase:firebase-messaging': '19.0.0'
             ],
-            // Tested up to play-services-basement:17.4.0
+            // Tested up to play-services-basement:17.5.0
         ]
     ]
 
